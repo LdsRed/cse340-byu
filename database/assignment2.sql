2953046322185be6b877261d1a84a62d794ce017
lds red <lds.larcher@gmail.com>
2025-01-18 18:31:59 -0300

    Updating Database file with assignment 2

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,4 +1,5 @@
--- INSERT A NEW VALUE TO THE ACCOUNT TABLE
+
+-- 1) INSERT A NEW VALUE TO THE ACCOUNT TABLE
 INSERT INTO public.account (account_firstname,
 							account_lastnmae,
 							account_email,
@@ -7,16 +8,40 @@ INSERT INTO public.account (account_firstname,
 VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n', 'Client');
 
 
--- ALTER OR MODIFY A VALUE IN THE ACCOUNT TABLE
+-- 2) Modify the Tony Stark record to change the account_type to "Admin"
 UPDATE public.account
 SET account_type = 'Admin'
-WHERE account_firstname = 'Tony' AND account_lastnmae = 'Start';
+WHERE account_id = ;
 
 
--- RETRIEVE USER DATA FROM THE ACCOUNT TABLE
-SELECT * FROM public.account;
-
--- DELETE A VALUE FROM THE ACCOUNT TABLE
+-- 3) Delete the Tony Stark record from the database.
 
 DELETE FROM public.account
 WHERE account_id = 1;
+
+-- 4) Modify the description of the record associated with inv_id of 10 to say "a huge interior"
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_id = 10;
+
+
+-- 5) Use Inner join to select two tables
+SELECT 
+	inventory.inv_make,
+	inventory.inv_model,
+	classification.classification_name
+FROM 
+	public.inventory
+INNER JOIN
+	public.classification
+ON
+	inventory.classification_id = classification.classification_id
+WHERE
+	classification.classification_name = 'Sport';
+
+-- 6) Modify the image and thumbnail for the record associated with inv_id of 10 to say "/images/vehicles"
+UPDATE public.inventory
+SET 
+inv_image = REPLACE(inv_image, '/images', '/images/vehicles'), 
+inv_thumbnail = REPLACE(inv_thumbnail, '/images', '/images/vehicles')
+WHERE inv_id = 10;
